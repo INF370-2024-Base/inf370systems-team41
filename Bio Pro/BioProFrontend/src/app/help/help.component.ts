f19f34355840ae47799ff2c7eb2cc171f0f950d4
u21499022 <u21499022@tuks.co.za>
2024-07-04 12:20:38 +0200

    styling

--- Bio Pro/BioProFrontend/src/app/help/help.component.ts
+++ Bio Pro/BioProFrontend/src/app/help/help.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-help',
@@ -12,4 +13,14 @@ export class HelpComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+}
 }
