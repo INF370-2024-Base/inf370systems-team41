ff5de7257dd8743fe18d2428c8177a20da4a0f8d
u21499022 <u21499022@tuks.co.za>
2024-07-07 13:29:51 +0200

    help and styling update

--- Bio Pro/BioProFrontend/src/app/help-deliveries/help-deliveries.component.ts
+++ Bio Pro/BioProFrontend/src/app/help-deliveries/help-deliveries.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help-deliveries.component.scss']
 })
 export class HelpDeliveriesComponent implements OnInit {
+  dropdownOpen: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+ 
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen; // Toggle the dropdown state
   }
 
 }
