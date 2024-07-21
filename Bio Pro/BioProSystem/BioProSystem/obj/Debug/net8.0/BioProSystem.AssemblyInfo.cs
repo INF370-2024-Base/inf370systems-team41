83f33d7efae2ba5894d5d23adc27ce48acc1fa3f
u22503171 <u22503171@tuks.co.za>
2024-07-21 19:22:16 +0200

    style

--- Bio Pro/BioProSystem/BioProSystem/obj/Debug/net8.0/BioProSystem.AssemblyInfo.cs
+++ Bio Pro/BioProSystem/BioProSystem/obj/Debug/net8.0/BioProSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BioProSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001cee1f52d6bce21db60432501b5715c9aad0ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa3b8d337cf003d613c33eb2c306efd942a4a92")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioProSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioProSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
