445f3339464d691dc65827040db731539705a03b
u21499022 <u21499022@tuks.co.za>
2024-09-18 16:25:04 +0200

    update password help section

--- Bio Pro/BioProFrontend/src/app/help-profile/help-profile.component.ts
+++ Bio Pro/BioProFrontend/src/app/help-profile/help-profile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+
 
 @Component({
   selector: 'app-help-profile',
@@ -7,9 +10,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpProfileComponent implements OnInit {
 
-  constructor() { }
+  searchQuery: string = '';
+  selectedFilter: string = 'all';
+  noResultsFound: boolean = false;
+
+  dropdownStates: { [key: string]: boolean } = {
+    steps: false,
+    commonQuestions: false,
+    technicalIssues: false
+  };
+
+  newDropdownStates: { [key: string]: boolean } = {
+    commonQuestions: false,
+    technicalIssues: false,
+    steps: false
+  };
+
+  constructor(private route: ActivatedRoute, private viewportScroller: ViewportScroller) {}
 
   ngOnInit(): void {
+    
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        this.viewportScroller.scrollToAnchor(fragment);
+      }
+    });
   }
 
+  @ViewChild('searchContent') searchContent!: ElementRef;
+
+  toggleDropdown(section: string) {
+    this.dropdownStates[section] = !this.dropdownStates[section];
+  }
+
+  toggleNewDropdown(section: string) {
+    this.newDropdownStates[section] = !this.newDropdownStates[section];
+  }
+
+  handleExpansionClick(event: Event) {
+    event.stopPropagation();
+  }
+
+  isAnySectionOpen(): boolean {
+    return Object.values(this.dropdownStates).some(state => state) || Object.values(this.newDropdownStates).some(state => state);
+  }
+  
+  onSearch(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.searchQuery = input.value.toLowerCase();
+    this.filterContent();
+    event.stopPropagation();
+  }
+
+  onFilter(event: any) {
+    this.selectedFilter = event.value;
+    this.filterContent();
+  }
+
+  shouldDisplayContainer(containerClass: string): boolean {
+    return this.selectedFilter === 'all' || this.selectedFilter === containerClass;
+  }
+
+  filterContent() {
+    const searchContentElement = this.searchContent.nativeElement;
+    const searchableElements = searchContentElement.querySelectorAll('.searchable');
+    let found = false;
+
+    searchableElements.forEach((element: any) => {
+      const textContent = element.textContent.toLowerCase();
+      if (this.searchQuery && textContent.includes(this.searchQuery)) {
+        element.style.display = '';
+        found = true;
+      } else {
+        element.style.display = 'none';
+      }
+    });
+
+    this.noResultsFound = this.searchQuery !== '' && !found;
+  }
 }
