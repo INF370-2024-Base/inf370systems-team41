dd08b0fab57bb44634b69f43db105d7aaf8a8eef
pieterschoeman05 <164881336+pieterschoeman05@users.noreply.github.com>
2024-09-25 21:29:58 +0200

    Fixed employee

--- Bio Pro/BioProSystem/BioProSystem/obj/Debug/net8.0/BioProSystem.AssemblyInfo.cs
+++ Bio Pro/BioProSystem/BioProSystem/obj/Debug/net8.0/BioProSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BioProSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c5471a051d217771b916834e7f087173505c4af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f5f59d34bfdcdaaa76b058f32722d102dc82c41")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioProSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioProSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
