aec6d487d3e971337b4e2eef59e0265a99bf3f8a
pieterschoeman05 <164881336+pieterschoeman05@users.noreply.github.com>
2024-10-24 16:31:04 +0200

    Fixed audit trail bug

--- Bio Pro/BioProSystem/BioProSystem/obj/Debug/net8.0/BioProSystem.AssemblyInfo.cs
+++ Bio Pro/BioProSystem/BioProSystem/obj/Debug/net8.0/BioProSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BioProSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+003829d2742acfe8b6f85899523ef0e68080ce1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fff7229d3ef708ff6ee60f95b3506e37214a502c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioProSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioProSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
